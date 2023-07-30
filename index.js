db9fba9984d59779866dd4a7ddcd3fd408346cb9
Dro5zd <adrobotyuk@gmail.com>
2023-07-30 15:10:08 +0500

    change link2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const http= require('http');
 const {Server} = require('socket.io');
 const cors = require("cors");
+const route = require('./route')
 const app = express();
 
 let activeSessions = 0;
 
 app.use(cors({origin: '*'}))
+app.use(route);
 
 const server = http.createServer(app);
 
